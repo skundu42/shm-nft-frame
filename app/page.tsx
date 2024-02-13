359dca4bdc90003664cf2196d9969e03df17de1e
skundu42 <skundu_be19@thapar.edu>
2024-02-13 16:01:43 +0530

    initial scripts

---
+++ app/page.tsx
@@ -0,0 +1,99 @@
+import {
+  FrameButton,
+  FrameContainer,
+  FrameImage,
+  FrameInput,
+  FrameReducer,
+  NextServerPageProps,
+  getPreviousFrame,
+  useFramesReducer,
+  getFrameMessage,
+} from "frames.js/next/server";
+import Link from "next/link";
+import { DEBUG_HUB_OPTIONS } from "./debug/constants";
+import { getTokenUrl } from "frames.js";
+
+type State = {
+  active: string;
+  total_button_presses: number;
+};
+
+const initialState = { active: "1", total_button_presses: 0 };
+
+const reducer: FrameReducer<State> = (state, action) => {
+  return {
+    total_button_presses: state.total_button_presses + 1,
+    active: action.postBody?.untrustedData.buttonIndex
+      ? String(action.postBody?.untrustedData.buttonIndex)
+      : "1",
+  };
+};
+
+// This is a react server component only
+export default async function Home({
+  params,
+  searchParams,
+}: NextServerPageProps) {
+  const previousFrame = getPreviousFrame<State>(searchParams);
+
+  const frameMessage = await getFrameMessage(previousFrame.postBody, {
+    ...DEBUG_HUB_OPTIONS,
+  });
+
+  if (frameMessage && !frameMessage?.isValid) {
+    throw new Error("Invalid frame payload");
+  }
+
+  const [state, dispatch] = useFramesReducer<State>(
+    reducer,
+    initialState,
+    previousFrame
+  );
+
+  // Here: do a server side side effect either sync or async (using await), such as minting an NFT if you want.
+  // example: load the users credentials & check they have an NFT
+
+  console.log("info: state is:", state);
+
+  if (frameMessage) {
+    const {
+      isValid,
+      buttonIndex,
+      inputText,
+      castId,
+      requesterFid,
+      casterFollowsRequester,
+      requesterFollowsCaster,
+      likedCast,
+      recastedCast,
+      requesterVerifiedAddresses,
+      requesterUserData,
+    } = frameMessage;
+
+    console.log("info: frameMessage is:", frameMessage);
+  }
+
+  const baseUrl = process.env.NEXT_PUBLIC_HOST || "http://localhost:3000";
+
+  // then, when done, return next frame
+  return (
+    <div className="p-4">
+      frames.js starter kit. The Template Frame is on this page, it&apos;s in
+      the html meta tags (inspect source).{" "}
+      <Link href={`/debug?url=${baseUrl}`} className="underline">
+        Debug
+      </Link>
+      <FrameContainer
+        postUrl="/frames"
+        state={state}
+        previousFrame={previousFrame}
+      >
+        {/* <FrameImage src="https://framesjs.org/og.png" /> */}
+        <FrameImage src="https://cdn.dribbble.com/users/383277/screenshots/18055765/media/e5fc935b60035305099554810357012a.png?compress=1&resize=400x300" />
+        <FrameButton action="link" target={`https://shardeum-nft-minting.vercel.app/`}>
+          Mint NFT on Shardeum
+        </FrameButton>
+      </FrameContainer>
+    </div>
+  );
+}
